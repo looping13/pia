96f03993fe709acc07c8ae3a03db418f1f9b520f
looping13 <104414920+looping13@users.noreply.github.com>
2023-07-14 23:21:34 +0200

    Update piaOneLiner.sh

--- piaOneLiner.sh
+++ piaOneLiner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /YOUR_PIA_SCRIPTS_LOCATION/manual-connections
+echo PIA startup - checking internet connectivity
+while ! ping -c 1 www.privateinternetaccess.com; do sleep 1; done
 sudo VPN_PROTOCOL=wireguard \
 DISABLE_IPV6=yes \
 DIP_TOKEN=no \
